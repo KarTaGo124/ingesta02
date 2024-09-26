c1c5e41a193b00d0063e0d256dc05b6386bccc74
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-09-26 12:22:34 -0500

    ingesta changes

--- ingesta.py
+++ ingesta.py
@@ -1,7 +1,31 @@
+import mysql.connector
+import csv
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+conexion = mysql.connector.connect(
+    host="18.213.155.42",
+    user="root",
+    password="utec",
+    database="tienda"
+)
+
+cursor = conexion.cursor()
+
+cursor.execute("SELECT * FROM fabricantes")
+
+with open('archivo.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    columnas = [i[0] for i in cursor.description]
+    csvwriter.writerow(columnas)
+    
+    for fila in cursor.fetchall():
+        csvwriter.writerow(fila)
+
+cursor.close()
+conexion.close()
+
+ficheroUpload = "archivo.csv"
+nombreBucket = "bucket-output-guillermo124"
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
